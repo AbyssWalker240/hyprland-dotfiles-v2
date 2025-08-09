2808cd5a3d210eee042fbef886751c492970b448
AbyssWalker240 <>
2025-08-09 14:18:27 -0400

    Added more funnies

--- core/.zsh_funnies.zsh
+++ core/.zsh_funnies.zsh
@@ -3,5 +3,9 @@ alias balls="figlet -f ~/git/figlet-fonts/3d BALLS | lolcat"
 alias cbt="figlet -f ~/git/figlet-fonts/3d 'Cock & Ball TORTURE' | lolcat"
 alias shit="figlet -f ~/git/figlet-fonts/3d 'SHIT' | lolcat"
 alias yeah="figlet -f ~/git/figlet-fonts/3d YEAH | lolcat"
+alias ok="figlet -f ~/git/figlet-fonts/3d OK | lolcat"
 alias snapstreaks="\clear && echo && richdate && figlet S -f git/figlet-fonts/3d | lolcat && echo"
 
+function loltext() {
+  figlet -f ~/git/figlet-fonts/3d "$*" | lolcat
+}
