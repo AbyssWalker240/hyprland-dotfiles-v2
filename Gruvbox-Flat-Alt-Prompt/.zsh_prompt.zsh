d6bb311654379a41011fe8ec24604008b1c32c89
AbyssWalker240 <>
2025-08-13 02:43:07 -0400

    Renamed packages to include numbers for easy switching in rofi

--- Gruvbox-Flat-Alt-Prompt/.zsh_prompt.zsh
+++
@@ -1,4 +0,0 @@
-PROMPT=$'%F{15}%B%n %b%f%F{#fe8019}%~%f
-%(?.%F{14}\U279C%f.%F{9}\U279C%f)  '
-# runner up: PROMPT=$'%F{8}\U250c\U2500%f%F{0}\U25d6%f%F{#fe8019}%K{0}%B%~%b%k%f%F{0}\U25d7%f 
-#%F{8}\U2514\U2574%f%(?.%F{14}\U276F%f.%F{9}\U276f%f)  '
