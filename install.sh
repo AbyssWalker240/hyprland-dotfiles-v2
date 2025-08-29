fcdc173688cf2a12dad60e1937059239b1b1de82
AbyssWalker240 <>
2025-08-29 03:52:07 -0400

    working on install script

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ command -v pacman > /dev/null || exit 1
 
 
 # Confirm with user
-if ! confirmation "${g}Hello${R}\n
+if ! confirmation "${g}\nHello${R}\n
 This is a script that installs my hyprland dotfiles onto an Arch Linux system.
 It is reccomended that you start on a fresh install of Arch.
 
@@ -36,19 +36,20 @@ ${g}Proceed?${R}
   echo -e "${r}User cancelled, aborting.${R}" && exit 1
 fi
 
-if ! confirmation "${r}Any changes made to '~/.config' will be DELETED!
-Proceed?${R}"; then
+if ! confirmation "${r}\nAny changes made to '~/.config' will be DELETED!
+Proceed?${R}
+"; then
   echo -e "${r}User cancelled, aborting.${R}" && exit 1
 fi
 
 
 # Initial update and dependancy install
-echo -e "${g}Updating system and installing dependencies...${R}"
+echo -e "${g}\nUpdating system and installing dependencies...${R}"
 sudo pacman -Syu --needed --noconfirm git
 
 
 # Build yay
-echo -e "${g}Building yay in a temporary directory...${R}"
+echo -e "${g}\nBuilding yay in a temporary directory...${R}"
 current_pid="$$"
 yay_dir="${HOME}/yay${current_pid}"
 
@@ -64,7 +65,7 @@ rm -rf "${yay_dir}"
 
 
 # Clone Dotfiles repo
-echo -e "${g}Cloning dotfiles repo...${R}"
+echo -e "${g}\nCloning dotfiles repo...${R}"
 git clone https://github.com/AbyssWalker240/hyprland-dotfiles-v2 Dotfiles
 cd "${HOME}/Dotfiles"
 
@@ -75,18 +76,17 @@ Depending on your internet connection, this may take a while.
 Proceed?"; then
   echo -e "${r}User cancelled, aborting.${R}" && exit 1
 fi
-sleep 3
 yay -S --needed --noconfirm - < package-list
 
 
 # Install oh-my-zsh
-echo -e "${g}Installing oh-my-zsh${R}"
+echo -e "${g}\nInstalling oh-my-zsh${R}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s "$(command -v zsh)" ${USER}
 
 
 # Initialize dotswap
-echo -e "${g}Stowing configs...${R}"
+echo -e "${g}\nStowing configs...${R}"
 rm -f "${HOME}/.zshrc"
 mkdir "${HOME}/Pictures"
 $HOME/Dotfiles/core/.config/hypr/scripts/dotswap.sh init
