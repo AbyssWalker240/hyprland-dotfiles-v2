6b2ac746de5130d371dea238442a73150fe750de
AbyssWalker240 <>
2026-08-12 11:07:20 -0400

    Added power option to kill waybar

--- core/.local/bin/scripts/rofi-utils/powerMenu.sh
+++ core/.local/bin/scripts/rofi-utils/powerMenu.sh
@@ -5,21 +5,16 @@ selection=$(echo "Sleep
 Log out
 Reboot
 Shutdown
-Shutdown Now" | rofi -dmenu -i -p "power" -lines 4 -theme $HOME/.config/rofi/themes/hyprpwrm.rasi 2> /dev/null)
+Exit waybar" | rofi -dmenu -i -p "power" -lines 4 -theme $HOME/.config/rofi/themes/hyprpwrm.rasi 2> /dev/null)
 
-if [[ $selection == "Shutdown" ]]; then
-	hyprctl notify 3 30000 "rgb(ff0000)" "Shutdown scheduled"
-	sleep 30
-	systemctl poweroff
-elif [[ $selection == "Reboot" ]]; then
+if [[ $selection == "Reboot" ]]; then
 	systemctl reboot
-elif [[ $selection == "Shutdown Now" ]]; then
+elif [[ $selection == "Shutdown" ]]; then
   systemctl poweroff
 elif [[ $selection == "Log out" ]]; then
 	loginctl terminate-user $USER
 elif [[ $selection == "Sleep" ]]; then
 	systemctl suspend
-else
-	hyprctl dismissnotify &> /dev/null
-  killall powerMenu.sh
+elif [[ $selection == "Exit waybar" ]]; then
+	killall waybar
 fi
